a5c7de1285d753e6695e8e07d7e6600ed1be383c
Wang,Zhe <zhe1.wang@intel.com>
2023-08-21 18:17:43 +0800

    dispatcher draft

---
+++ q_bits/cscr/jblas_task_dispatcher.hpp
@@ -0,0 +1,106 @@
+#include <torch/torch.h>
+#include "jblas/jit_blas_weight_compression.h"
+
+#define INTERFACE_TEMPLATE                                            \
+  template <class _Launcher_T, template <class _T> class _Parallel_T> \
+  class Interface
+#define LAUNCHER_TEMPLATE                                                                              \
+  template <JBLAS_ISA _RT_ISA_T, class _GemmCore_T, template <class _T, JBLAS_ISA> class _PrologueA_T, \
+            template <class _T, JBLAS_ISA> class _PrologueB_T, template <JBLAS_ISA> class _Epilogue_T> \
+  class Launcher
+
+enum QBITS_TASK {
+  QBITS_QUANTIZE,
+  QBITS_DEQUANTIZE,
+  QBITS_LINEAR,
+};
+
+enum QBITS_DT {
+  QBITS_FP32,
+  QBITS_BF16,
+  QBITS_FP16,
+};
+
+inline bool check_amx() { return jblas::utils::parallel::CpuDevice::getInstance()->AMX_BF16(); }
+inline bool check_vnni() { return jblas::utils::parallel::CpuDevice::getInstance()->AVX_VNNI(); }
+inline bool check_avx512f() { return jblas::utils::parallel::CpuDevice::getInstance()->AVX512F(); }
+
+struct jblas_config_param {
+  std::string compute_type;  // determin gemm core template
+  std::string weight_type;   // determin compress-weight template
+  QBITS_DT src_dt;           // determin activation related template
+  QBITS_DT dst_dt;           // determin write_back template
+};
+
+struct qbits_runtime_ctx {
+  torch::Tensor *activation, *weight, *bias, *output;
+  bool transpose;
+  int64_t blocksize, m, n, k, lda, ldo;
+};
+
+template <QBITS_TASK TASK, class KERNEL>
+void execute_task(jblas_config_param* p, qbits_runtime_ctx* ctx) {}
+
+template <QBITS_TASK TASK, INTERFACE_TEMPLATE, LAUNCHER_TEMPLATE, class Gemmcore, template <class _T> class Parallel,
+          JBLAS_ISA ISA, template <class _T, JBLAS_ISA> class PrologueB, template <class _T, JBLAS_ISA> class PrologueA>
+void parse_store(jblas_config_param* p, qbits_runtime_ctx* ctx) {
+  if (p->dst_dt == QBITS_FP32) {
+    return execute_task<
+        TASK, Interface<Launcher<ISA, Gemmcore, PrologueA, PrologueB, jblas::epilogue::gemm::AccumulatorWriteBackFp32>,
+                        Parallel>>(p, ctx);
+  }
+  TORCH_CHECK(false, "unsupported dst data type.");
+}
+
+template <QBITS_TASK TASK, INTERFACE_TEMPLATE, LAUNCHER_TEMPLATE, class Gemmcore, template <class _T> class Parallel,
+          JBLAS_ISA ISA, template <class _T, JBLAS_ISA> class PrologueB>
+void parse_activation(jblas_config_param* p, qbits_runtime_ctx* ctx) {
+  if (p->compute_type == "int8" && p->src_dt == QBITS_FP32 && check_amx()) {
+    return parse_store<TASK, Interface, Launcher, Gemmcore, Parallel, ISA, PrologueB,
+                       jblas::prologue::gemm::ActivationF32S8KBlockQuantize>(p, ctx);
+  }
+  TORCH_CHECK(false, "unsupported src data type.");
+}
+
+template <QBITS_TASK TASK, INTERFACE_TEMPLATE, LAUNCHER_TEMPLATE, class Gemmcore, template <class _T> class Parallel,
+          JBLAS_ISA ISA>
+void parse_weight(jblas_config_param* p, qbits_runtime_ctx* ctx) {
+  if (p->weight_type == "s8_scalef32") {
+    return parse_activation<TASK, Interface, Launcher, Gemmcore, Parallel, ISA,
+                            jblas::prologue::weight_comp::gemm_kblcok::WeightS8ScaleFp32>(p, ctx);
+  }
+  if (p->weight_type == "s4clip_scalef32") {
+  }
+  if (p->weight_type == "s4fullrange_scalef32") {
+  }
+  TORCH_CHECK(false, "unsupported weight_type, weight_type==" + p->weight_type);
+}
+
+template <QBITS_TASK TASK>
+void parse_gemm_core(jblas_config_param* p, qbits_runtime_ctx* ctx) {
+  if (p->compute_type == "int8") {
+    if (check_amx()) {
+      return parse_weight<TASK, jblas::wrapper::gemm_kblock::GemmInterfaceKBlockPackWeight,
+                          jblas::wrapper::gemm_kblock::GemmSLauncherKBlockPackWeight,
+                          jblas::gemm::kblock::GemmCore_Row_NN_16x48_AMX_INT8_KBLOCK,
+                          jblas::utils::parallel::Parallel2DGemmKBlockFixed, JblasAMX_INT8>(p, ctx);
+    }
+    if (check_vnni()) {
+      return parse_weight<TASK, jblas::wrapper::gemm_kblock::GemmInterfaceKBlockPackWeight,
+                          jblas::wrapper::gemm_kblock::GemmSLauncherKBlockPackWeight,
+                          jblas::gemm::kblock::GemmCore_Row_NN_3x48_AVX512_VNNI_KBLOCK,
+                          jblas::utils::parallel::Parallel2DGemmKBlockFixed, JblasAVX512_VNNI>(p, ctx);
+    }
+    TORCH_CHECK(false, "device ISA muster lagger than VNNI when compute_type==int8");
+  }
+  if (p->compute_type == "fp32") {
+  }
+  if (p->compute_type == "bf16") {
+  }
+  TORCH_CHECK(false, "unsupported compute_type, compute_type==" + p->compute_type);
+}
+
+template <QBITS_TASK TASK>
+void task_dispatcher(jblas_config_param* p, qbits_runtime_ctx* ctx) {
+  return parse_gemm_core<TASK>(p, ctx);
+}
--- q_bits/cscr/weight_only_jblasop.cpp
+++ q_bits/cscr/weight_only_jblasop.cpp
@@ -1,28 +1,41 @@
 #include <torch/script.h>
 #include <torch/torch.h>
+#include "jblas_task_dispatcher.hpp"
+
+template <QBITS_DT SRC_DT, QBITS_DT DST_DT>
+static void inline init_jblas_config_param(jblas_config_param* p, const std::string& compute_type,
+                                           const std::string& weight_type) {
+  p->compute_type = compute_type;
+  p->weight_type = weight_type;
+  p->src_dt = SRC_DT;
+  p->dst_dt = DST_DT;
+}
 
 static torch::Tensor jblas_quantize(const torch::Tensor& fp32_weight, bool transpose, int64_t block_size,
-                                    const std::string& compute_type, const std::string& quant_type,
-                                    const std::string& scale_type) {}
+                                    const std::string& compute_type, const std::string& weight_type) {
+  torch::Tensor output;
+  jblas_config_param p;
+  init_jblas_config_param<QBITS_FP32, QBITS_FP32>(&p, compute_type, weight_type);
+  qbits_runtime_ctx ctx{nullptr, const_cast<torch::Tensor*>(&fp32_weight), nullptr, &output, transpose, block_size};
+  task_dispatcher<QBITS_QUANTIZE>(&p, &ctx);
+  return output;
+}
 
 static void jblas_dequantize_weight(torch::Tensor& compressed_weight, torch::Tensor& dequantize_weight, bool transpose,
-                                    const std::string& compute_type, const std::string& quant_type,
-                                    const std::string& scale_type) {}
+                                    const std::string& compute_type, const std::string& weight_type) {}
 
-static void jblas_quantweight_f32_linear_with_bias(const torch::Tensor& activation, const torch::Tensor& weight,
-                                                   const torch::Tensor& bias, torch::Tensor& output, int64_t m,
-                                                   int64_t n, int64_t k, int64_t lda, int64_t ldo,
-                                                   const std::string& compute_type, const std::string& quant_type,
-                                                   const std::string& scale_type) {}
+static void jblas_f32in_f32out_linear_with_bias(const torch::Tensor& activation, const torch::Tensor& weight,
+                                                const torch::Tensor& bias, torch::Tensor& output, int64_t lda,
+                                                int64_t ldo, const std::string& compute_type,
+                                                const std::string& weight_type) {}
 
-static void jblas_quantweight_f32_linear_without_bias(const torch::Tensor& activation, const torch::Tensor& weight,
-                                                      torch::Tensor& output, int64_t m, int64_t n, int64_t k,
-                                                      int64_t lda, int64_t ldo, const std::string& compute_type,
-                                                      const std::string& quant_type, const std::string& scale_type) {}
+static void jblas_f32in_f32out_linear_without_bias(const torch::Tensor& activation, const torch::Tensor& weight,
+                                                   torch::Tensor& output, int64_t n, int64_t lda, int64_t ldo,
+                                                   const std::string& compute_type, const std::string& weight_type) {}
 
 TORCH_LIBRARY(weight_only_jblasop, m) {
   m.def("jblas_quantize", &jblas_quantize);
-  m.def("jblas_quantweight_f32_linear_with_bias", &jblas_quantweight_f32_linear_with_bias);
-  m.def("jblas_quantweight_f32_linear_without_bias", &jblas_quantweight_f32_linear_without_bias);
+  m.def("jblas_quantweight_f32inf32out_linear_with_bias", &jblas_f32in_f32out_linear_with_bias);
+  m.def("jblas_quantweight_f32inf32out_linear_without_bias", &jblas_f32in_f32out_linear_without_bias);
   m.def("jblas_dequantize_weight", &jblas_dequantize_weight);
 }
